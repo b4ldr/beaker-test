9abe64d2ccf57766c3675d7f3dddc09a0b2e931f
John Bond <github@johnbond.org>
2018-03-16 15:31:36 +0000

    update

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -4,14 +4,15 @@ require 'beaker-rspec'
 require 'progressbar'
 
 
-puppet_install_script = 'https://iad01.master.dns.icann.org:8140/packages/current/install.bash'
+PUPPET_INSTALL_SCRIPT = 'https://iad01.master.dns.icann.org:8140/packages/current/install.bash'
+AUTOSIGN_PASSWORD     = '2?h}8xtE}PyLXsY+=d2!'
 
 # Install Puppet on all hosts
 hosts.each do |host|
   step "install packages on #{host}"
   on(host, 'printf "nameserver 8.8.8.8" > /etc/resolv.conf')
   step 'install puppet'
-  on(host, "/usr/bin/curl -k #{puppet_install_script} | /bin/bash") 
+  on(host, "/usr/bin/curl -k #{PUPPET_INSTALL_SCRIPT} | /bin/bash -s custom_attributes:challengePassword=#{AUTOSIGN_PASSWORD}") 
 end
 RSpec.configure do |c|
   c.formatter = :documentation
