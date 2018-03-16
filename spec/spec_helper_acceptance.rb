8a394ef6461b16444204a3686ba209e891b4d2f6
John Bond <github@johnbond.org>
2018-03-16 13:10:00 +0000

    add install script

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -4,6 +4,8 @@ require 'beaker-rspec'
 require 'progressbar'
 
 
+puppet_install_script = 'https://iad01.master.dns.icann.org:8140/packages/current/install.bash'
+
 # Install Puppet on all hosts
 hosts.each do |host|
   step "install packages on #{host}"
@@ -11,6 +13,7 @@ hosts.each do |host|
   # remove search list and domain from resolve.conf
   on(host, 'printf "nameserver 8.8.8.8" > /etc/resolv.conf')
   step 'install puppet'
+  on(host, "/usr/bin/curl -k #{puppet_install_script} | /bin/bash") 
 end
 RSpec.configure do |c|
   c.formatter = :documentation
