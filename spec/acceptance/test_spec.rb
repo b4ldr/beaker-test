1e96a7562951fcad712b491cb8beebdc757e5c31
John Bond <github@johnbond.org>
2018-03-16 17:31:20 +0000

    update

--- spec/acceptance/test_spec.rb
+++ spec/acceptance/test_spec.rb
@@ -4,7 +4,7 @@ require 'spec_helper_acceptance'
 
 describe 'puppet install' do
   context 'defaults' do
-    let(:args) { '--no-daemonize -v -t -o -e 206_genral_erros' }
+    let(:args) { '--no-daemonize -v -t -o --environment 206_genral_erros' }
     it 'is_expected.to work with no errors' do
       run_agent_on(default, args)
     end
