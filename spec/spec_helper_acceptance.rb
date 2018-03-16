590645dce589289e53bb535f3a188d58ec3193a5
John Bond <github@johnbond.org>
2018-03-16 16:02:55 +0000

    update paths

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -6,15 +6,15 @@ require 'progressbar'
 
 PUPPET_INSTALL_SCRIPT = 'https://iad01.master.dns.icann.org:8140/packages/current/install.bash'
 AUTOSIGN_PASSWORD     = '2?h}8xtE}PyLXsY+=d2!'
-
+SCRIPT_DIR = File.expand_path(File.dirname(__FILE__))
 # Install Puppet on all hosts
 hosts.each do |host|
   step "Update nameserver on #{host}"
   on(host, 'printf "nameserver 8.8.8.8" > /etc/resolv.conf')
   step "install puppet on #{host}"
   on(host, "/usr/bin/curl -k #{PUPPET_INSTALL_SCRIPT} | /bin/bash -s custom_attributes:challengePassword=#{AUTOSIGN_PASSWORD}") 
-  cert_source = File.join(Dir.pwd, '..', 'ca', "#{host}.pem")
-  key_source = File.join(Dir.pwd, '..', 'ca', "#{host}.key")
+  cert_source = File.join(SCRIPT_DIR, '..', 'ca', "#{host}.pem")
+  key_source = File.join(SCRIPT_DIR, '..', 'ca', "#{host}.key")
   cert_dest = File.join(host.puppet['ssldir'], 'certs', "#{host}.pem")
   key_dest = File.join(host.puppet['ssldir'], 'private_keys', "#{host}.pem")
   step "copy public key #{host}"
