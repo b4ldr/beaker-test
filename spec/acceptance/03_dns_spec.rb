c43230cacab18b3630164dc72b734716a510f07d
John Bond <github@johnbond.org>
2018-04-06 15:52:22 +0100

    add dns tests

---
+++ spec/acceptance/03_dns_spec.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+if default['roles'].include?('dns')
+
+  require 'spec_helper_acceptance'
+
+  describe 'Testing routing configueration' do
+    default['dns'].each_pair do |zone_set, config|
+      context "check #{zone_set} Zone Set @#{config['address']}" do
+        describe service(config['service']) do
+          it { is_expected.to be_running }
+        end
+        describe port(53) do
+          it { is_expected.to be_listening }
+        end
+        config['zones'].each do |zone| 
+          describe command("digi +short soa #{zone} @#{config['address']}") do
+            its(:stdout) do
+              is_expected.to match(
+                %r{[a-z\-\.]+\s[a-z\-\.]+\s\d+\s\d+\s\d+\s\d+\s\d+} 
+              )
+            end
+          end
+        end
+      end
+    end
+  end
+end
--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -8,19 +8,23 @@ PUPPET_INSTALL_SCRIPT = 'https://iad01.master.dns.icann.org:8140/packages/curren
 SCRIPT_DIR = File.expand_path(File.dirname(__FILE__))
 # Install Puppet on all hosts
 hosts.each do |host|
-  step "Update nameserver on #{host}"
+  step "#{host}: Install apparmor-utils on"
+  host.install_package('apparmor-utils')
+  step "#{host}: disable App armor for named"
+  on(host, 'aa-disable /usr/sbin/named')
+  step "#{host}: Update nameserver on"
   on(host, 'printf "nameserver 8.8.8.8" > /etc/resolv.conf')
-  step "install puppet on #{host}"
+  step "#{host}: install puppet on"
   on(host, "/usr/bin/curl -k #{PUPPET_INSTALL_SCRIPT} | /bin/bash -s") 
   cert_source = File.join(SCRIPT_DIR, '..', 'ca', "#{host}.pem")
   key_source = File.join(SCRIPT_DIR, '..', 'ca', "#{host}.key")
   cert_dest = File.join(host.puppet['ssldir'], 'certs', "#{host}.pem")
   key_dest = File.join(host.puppet['ssldir'], 'private_keys', "#{host}.pem")
-  step "delete #{cert_dest} and #{key_dest} generated during install"
+  step "#{host}: delete #{cert_dest} and #{key_dest} generated during install"
   on(host, "rm -rf #{cert_dest} #{key_dest}")
-  step "copy public key #{host}"
+  step "#{host}: copy public key"
   scp_to(host, cert_source, cert_dest)
-  step "copy private key #{host}"
+  step "#{host}: copy private key"
   scp_to(host, key_source, key_dest)
 end
 RSpec.configure do |c|
